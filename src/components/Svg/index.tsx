7ffda63d8739d252c57968cb2dfa41436a7aba8d
erwin-huang <erwinhuank@gmail.com>
2022-08-01 16:06:59 +0700

    feat: add facebook icon

--- src/components/Svg/index.tsx
+++ src/components/Svg/index.tsx
@@ -22,6 +22,7 @@ export { default as CogIcon } from "./Icons/Cog";
 export { default as CommunityIcon } from "./Icons/Community";
 export { default as CopyIcon } from "./Icons/Copy";
 export { default as ErrorIcon } from "./Icons/Error";
+export { default as FacebookIcon } from "./Icons/Facebook";
 export { default as HelpIcon } from "./Icons/Help";
 export { default as ImageAddIcon } from "./Icons/ImageAdd";
 export { default as InfoIcon } from "./Icons/Info";
