60d97a42c251a92944d95b8559ae7f211e3c83c2
sxio <stephen.huang96@gmail.com>
2022-12-01 14:43:51 +0700

    feat: remove dist, add dist to gitignore

--- dist/components/ButtonMenu/ButtonMenuWrapper.d.ts
+++
@@ -1,2 +0,0 @@
-declare const ButtonMenuWrapper: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, import("../Box").BoxProps, never>;
-export default ButtonMenuWrapper;
